0d1bc4db23dbca21561c7995ca01230784d0773c
John David <110695074+jdmurillos@users.noreply.github.com>
2023-03-21 11:37:48 -0500

    Add files via upload

---
+++ Ejercicios_practicos_python/POO_encapsulacion_2.py
@@ -0,0 +1,46 @@
+class Coordinate:
+    def __init__(self, x, y):
+        self.__X = x
+        self.__Y = y
+    def GetX(self):
+        return self.__X
+    def GetY(self):
+        return self.__Y
+    def SetX(self,x):
+        self.__X = x
+    def SetY(self,y):
+        self.__Y = y
+
+class Square:
+    def __init__(self,v1,v2,v3,v4):
+        self.__V1=v1
+        self.__V2=v2
+        self.__V3=v3
+        self.__V4=v4
+
+    def __ShowCoordinateV1(self):
+        print("(",self.__V1.GetX(),",",self.__V1.GetY(),")")
+    def __ShowCoordinateV2(self):
+        print("(",self.__V2.GetX(),",",self.__V2.GetY(),")")
+    def __ShowCoordinateV3(self):
+        print("(",self.__V3.GetX(),",",self.__V3.GetY(),")")
+    def __ShowCoordinateV4(self):
+        print("(",self.__V4.GetX(),",",self.__V4.GetY(),")")
+    def ShowVertex(self):
+        print("El cuadrado está compuesto por los siguientes vértices: ")
+        self.__ShowCoordinateV1()
+        self.__ShowCoordinateV2()
+        self.__ShowCoordinateV3()
+        self.__ShowCoordinateV4()
+
+v1=Coordinate(1,1)
+v2=Coordinate(4,1)
+v3=Coordinate(4,4)
+v4=Coordinate(1,4)
+
+square=Square(v1,v2,v3,v4)
+square.ShowVertex()
+
+
+
+    
