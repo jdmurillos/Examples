ef63b6d919b26ec87254691d575a9a8c267638a8
John David <110695074+jdmurillos@users.noreply.github.com>
2023-03-23 18:38:16 -0500

    Add files via upload

---
+++ Ejercicios_practicos_python/costo_terrenos.py
@@ -0,0 +1,15 @@
+class terreno:
+    def __init__(self,base,ancho,costo):
+        self.Base=base
+        self.Ancho=ancho
+        self.Costo=costo
+    
+    def MostrarCosto(self):
+        print("Largo del terreno es: ",str(self.Base))
+        print("Ancho del terreno es: ",str(self.Ancho))
+        print("Costo del metro cuadrado: ",str(self.Costo))
+        print("Costo del terreno es: ",str(self.Ancho * self.Base * self.Costo))
+    
+
+costo_total = terreno(3,4,1000)
+costo_total.MostrarCosto()
